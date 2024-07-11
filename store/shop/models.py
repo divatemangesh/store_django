d44f03caaa300d840b951ff88dcc8d25f52985d5
Mangesh Divate <mangesh@sangnak.in>
2024-07-12 00:57:35 +0530

    phone number uncommneted with correcton

--- store/shop/models.py
+++ store/shop/models.py
@@ -12,5 +12,5 @@ class Customer(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(max_length=255)
-    #phone = models.harField(max_length=255)
+    phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
